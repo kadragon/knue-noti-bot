87e4b20447477a4c6a1225be3cb1329de704aebc
kangdonguk <kangdongouk@gmail.com>
2024-10-12 20:03:02 +0900

    feat: Refactor Checker class for dependency injection and separation of concerns

--- src/checker.py
+++ src/checker.py
@@ -3,39 +3,59 @@ from src.rss import RSSFeedParser
 
 
 class Checker:
-    def __init__(self) -> None:
-        self.gist_manager = GistManager()
+    def __init__(self,
+                 gist_manager: GistManager,
+                 rss_parser: RSSFeedParser) -> None:
+        self.gist_manager = gist_manager
+        self.rss_parser = rss_parser
+        self.gist_data = {}
+
+    def load_gist_data(self):
         self.gist_data = self.gist_manager.get_gist_content()
 
-    def update_checker(self, update_flag: bool = False) -> list:
-        parser = RSSFeedParser()
+    def check_new_entries(self, idx, latest_article_url):
+        entries = self.rss_parser.parse_entries(idx)
+        new_entries = []
 
+        for entry in entries:
+            if entry['link'] != latest_article_url:
+                new_entries.append(entry)
+            else:
+                break
+        return new_entries
+
+    def generate_updated_content(self, recode, new_entries):
+        updated_content = []
+
+        for idx in recode.keys():
+            latest_article_url = new_entries[idx][0]['link'] if idx in new_entries else recode[idx]['url']
+            target = recode[idx]['target']
+            updated_content.append(f"{idx},{latest_article_url},{target}")
+        return updated_content
+
+    def update_checker(self, update_flag: bool = False) -> list:
+        self.load_gist_data()
         new_entries = []
         updated_data = {}
-
         is_data_updated = False
 
-        for site_id in self.gist_data.keys():
+        for site_id, recode in self.gist_data.items():
             gist_filename = f'{site_id}.csv'
-            updated_content = []
-
-            recode = self.gist_data[site_id]
+            site_new_entries = {}
 
             for idx in recode.keys():
-                latest_article_url = self.gist_data[site_id][idx]['url']
-                target = self.gist_data[site_id][idx]['target']
+                latest_article_url = recode[idx]['url']
+                target = recode[idx]['target']
+                entries = self.check_new_entries(idx, latest_article_url)
 
-                entries = parser.parse_entries(idx)
-
-                updated_content.append(f"{idx},{entries[0]['link']},{target}")
-
-                for entry in entries:
-                    if entry['link'] != latest_article_url:
+                if entries:
+                    site_new_entries[idx] = entries
+                    for entry in entries:
                         new_entries.append({'entry': entry, 'target': target})
-                        is_data_updated = True
-                    else:
-                        break
+                    is_data_updated = True
 
+            updated_content = self.generate_updated_content(
+                recode, site_new_entries)
             updated_data[gist_filename] = {
                 'content': f"""bbs_no,latest_article_url,notification_target\n{'\n'.join(updated_content)}"""
             }
